30e790e3b7db7df93efa15cae649e66f97a4e118
Roman Titov <ffuugoo@users.noreply.github.com>
2025-01-02 19:45:26 +0100

    Add `clear-cache.sh` script

---
+++ .local/bin/clear-cache.sh
@@ -0,0 +1,88 @@
+#!/bin/zsh
+
+set -euo pipefail
+
+function main {
+    for target in ${@:-min}; do
+        $target
+    done
+}
+
+function all {
+    zed
+    sublime
+    vscode
+    sccache
+    cargo
+    npm
+    zcomet
+    zsh
+}
+
+function dev {
+    sccache
+    cargo
+    npm
+    zsh
+}
+
+function min {
+    npm
+    zsh
+}
+
+function zed {
+    - rm -rf \
+        ~/Library/Application\ Support/Zed/{db,languages,node,prettier} \
+        ~/Library/Caches/Zed \
+        ~/.cache/zed
+}
+
+function sublime {
+    sublime-text
+    sublime-merge
+}
+
+function sublime-text {
+    - rm -rf ~/Library/Caches/Sublime\ Text
+}
+
+function sublime-merge {
+    - rm -rf ~/Library/Caches/Sublime\ Merge
+}
+
+function vscode {
+    :
+}
+
+function sccache {
+    - rm -rf ~/Library/Caches/Mozilla.sccache
+}
+
+function cargo {
+    - rm -rf \
+        ~/.cargo/registry \
+        ~/.cargo/git
+}
+
+function npm {
+    - rm -rf ~/.npm
+}
+
+function zcomet {
+    - rm -rf ~/.zcomet
+}
+
+function zsh {
+    - rm -rf \
+        ~/.zcompdump \
+        ~/.zcompdump.zwc \
+        ~/.zcompcache \
+        ~/.cache/fsh
+}
+
+function - {
+    ${DRY_RUN:+echo} $@
+}
+
+main $@
